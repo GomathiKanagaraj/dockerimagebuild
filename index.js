95d7b2687525fb5341c41d32a62b79dfb9a35ff2
GomathiKanagaraj <92990616+GomathiKanagaraj@users.noreply.github.com>
2024-01-04 11:12:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, Docker image!');
+  res.send('Hello, Docker image log file!');
 });
 
 app.listen(port, () => {
